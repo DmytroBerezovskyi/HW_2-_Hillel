c8accf62f5d71775c4af46bd9d2ed48f82ad2906
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2023-05-23 15:11:57 +0200

    add hw.csv

--- hw2.py
+++ hw2.py
@@ -1,5 +1,6 @@
 import requests
 from faker import Faker
+import csv
 
 fake = Faker()
 quantity_of_people = 100
@@ -20,3 +21,23 @@ if __name__ == '__main__':
 r = requests.get('http://api.open-notify.org/astros.json')
 if __name__ == '__main__':
     print(f'Number of astronauts: {r.json()["number"]}')
+
+
+with open('hw.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    height_cm = 0
+    weight_kg = 0
+    INCH = 2.54
+    POUND = 0.45359237
+    for row in reader:
+        inch_to_cm = float(row[' "Height(Inches)"']) * INCH
+        height_cm += inch_to_cm
+        pounds_to_kg = float(row[' "Weight(Pounds)"']) * POUND
+        weight_kg += pounds_to_kg
+        last_index = row['Index']
+
+avegare_height = height_cm / int(last_index)
+avegare_weight = weight_kg / int(last_index)
+print(avegare_height)
+print(avegare_weight)
+
